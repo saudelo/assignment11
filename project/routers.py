0db8b1ebe92242421f45898a7c159151a61569f5
Naoto Shimizu <naotoshimizu1@gmail.com>
2026-06-19 11:11:02 -0700

    new changes

--- project/routers.py
+++ project/routers.py
@@ -6,8 +6,8 @@
 
 from fastapi import APIRouter, Depends
 from auth import get_current_user
-from storage import read_travel_logs, write_data, TRAVEL_LOG_FILE, read_data, get_username_travel_log
-from models import TravelLogCreate, TravelLogResponse, TravelLogBase
+from storage import LOGIN_FILE, TRAVEL_LOG_FILE, read_travel_logs, write_data, read_data, get_username_travel_log, read_login_info
+from models import TravelLogCreate, TravelLogResponse, TravelLogBase, LoginInfo
 
 
 router = APIRouter()
@@ -18,9 +18,9 @@ def get_user(username: str = Depends(get_current_user)):
     return f"status: authenticated, username: {username}"
 
 #get all user logs
-@router.get("/travel_logs/", response_model = TravelLogBase )
-def get_all_logs():
-    return f"{      }"
+# @router.get("/travel_logs/", response_model = TravelLogBase )
+# def get_all_logs():
+#     return f"{      }"
 
 
 #test
@@ -37,6 +37,7 @@ def create_travel_log(travel_log: TravelLogCreate, username: str = Depends(get_c
     new_log = {
         "id": next_id,
         "username": travel_log.username,
+        "name": travel_log.name,
         "destination": travel_log.destination,
         "start_date": str(travel_log.start_date),
         "end_date": str(travel_log.end_date),
@@ -47,3 +48,17 @@ def create_travel_log(travel_log: TravelLogCreate, username: str = Depends(get_c
     write_data(TRAVEL_LOG_FILE, logs)
 
     return new_log
+
+@router.post("/user")
+def create_user_info(user: LoginInfo):
+    users = read_login_info()
+
+    new_user = {
+        "username": user.username,
+        "password": user.password
+    }
+
+    users.append(new_user)
+    write_data(LOGIN_FILE, users)
+
+    return new_user
